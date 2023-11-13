02a3f1d98482980a81f515bacbf99ba5e60691b6
633jinn <judyg00dy68@gmail.com>
2023-11-13 21:10:42 +0900

    feature: 프로젝트시작

---
+++ src/main/java/com/letsRoll/letsRoll/LetsRollApplication.java
@@ -0,0 +1,13 @@
+package com.letsRoll.letsRoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsRollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsRollApplication.class, args);
+	}
+
+}
