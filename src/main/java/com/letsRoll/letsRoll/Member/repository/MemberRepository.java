52e602cfd5f945729f9578f987ff37ef1f7e963e
Jongbeom Lee <116253832+JongbeomLee623@users.noreply.github.com>
2023-11-18 23:54:45 +0900

    Revert "Feature/projects"

--- src/main/java/com/letsRoll/letsRoll/Member/repository/MemberRepository.java
+++ src/main/java/com/letsRoll/letsRoll/Member/repository/MemberRepository.java
@@ -18,7 +18,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findMemberByProjectAndUserId(Project project, Long userId);
 
-    Optional<Member> findByProjectAndUser(Project project, User user);
     List<Member> findByProject(Project project);
     List<Member> findMembersByUser(User user);
 
